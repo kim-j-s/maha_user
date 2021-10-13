a014cfd70a6f42cf8f75980757b2a744325b107c
kogwing <kogwing@gmail.com>
2021-10-13 09:19:11 +0900

    가이드 수정

--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,26 @@
 (function($){
 
+    function inputChange() {
+        const $inputWrap = $('.inputWrap');
+        $inputWrap.each(function(e){
+            if($(this).find('.inp').val().length >= 1) {
+                $(this).find('.inp').addClass('on');
+            } else {
+                $(this).find('.inp').removeClass('on');
+            }
+
+            $(this).find('.inp').change(function(){
+                if($(this).val().length >= 1) {
+                    $(this).addClass('on');
+                } else {
+                    $(this).removeClass('on');
+                }
+            });
+        });
+    }
+    
     $(function(){
-        console.log('common.js');
-        $("#datepicker").datepicker();
+        inputChange();
+        // script
     });
 })(jQuery);
